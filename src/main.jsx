e1a984219fa285fb0bfa5f451f615945a373c241
Tannu Saxena <tannusaxena02@gmail.com>
2025-07-11 18:45:45 +0530

    .

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,9 @@ import { GoogleOAuthProvider } from '@react-oauth/google'
  const clientId = "627812142753-bde51a0nu2cop3ji87n7qrd3tqlkok9j.apps.googleusercontent.com";
 
 createRoot(document.getElementById('root')).render(
-  // <StrictMode>
+  <StrictMode>
   <GoogleOAuthProvider clientId={clientId}>
    <App />
   </GoogleOAuthProvider>
- 
-  // </StrictMode>,
+  </StrictMode>,
 )
